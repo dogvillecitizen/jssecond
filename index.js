522b9e86e4bba2e80e3aafcd1a57e5d8b027a6a4
Ia Sichinava <78296772+dogvillecitizen@users.noreply.github.com>
2022-04-14 16:18:06 +0400

    davaleba

--- index.js
+++ index.js
@@ -1,59 +1,71 @@
-// N1 davaleba ????????????????
+// 1. დაწერეთ ფუნქცია, რომელიც პარამეტრად მიიღებს ერთ მთელ რიცხვს და დააბრუნებს true -ს თუ რიცხვი არის ლუწი ან false -ს თუ რიცხვი არის კენტი.
 
-const currentDay = new Date().getDay();
-
-switch (currentDay) {
-  case 0:
-    console.log("sun");
-    break;
-  case 1:
-    console.log("mon");
-    break;
-  case 2:
-    console.log("tue");
-    break;
-  case 3:
-    console.log("wed");
-    break;
-  case 4:
-    console.log("thur");
-    break;
-  case 5:
-    console.log("fri");
-    break;
-  case 6:
-    console.log("sat");
-    break;
-
-  default:
-    console.log("not found");
-    break;
+function oddOrEven(a) {
+  if (oddOrEven % 2 == 0) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
 }
+oddOrEven(7);
 
-// N2 davaleba
+// 2. დაწერეთ ფუნქცია, რომელიც პარამეტრად მიიღებს ორ რიცხვს - მართკუთხედის გვერდების ზომებს და დააბრუნებს მართკუთხედის ფართობს.
 
-for (var i = 0; i < 100; i++) {
-  console.log(i);
+function rectArea(a, b) {
+  console.log(`rectangle  area - ${a * b}`);
 }
-// N3 davaleba
+rectArea(3, 5);
 
-var i = 0;
-while (i < 50) {
-  console.log(i);
-  i++;
+// // 3. დაწერეთ ფუქცია, რომელიც პარამეტრად მიიღებს ერთ რიცხვს - კვადრატის გვერდის სიგრძეს და
+// დააბრუნებს კვადრატის ფართობს. ფართობის გამოსათვლელად გამოიყენეთ #2 პუნქტში აღწერილი ფუნქცია.
+
+function square(a) {
+  console.log(`square area - ${a * a}`);
 }
-// N4 davaleba
+square(3, 5);
 
-do {
-  i++;
-  console.log(i);
-} while (i < 150);
+// 4 დაწერეთ ფუნქცია, რომელიც Math.random() ფუნქციის გამოყენებით დააგენერირებს და დააბრუნებს 0-დან 100-მდე შემთხვევით რიცხვს.
 
-// N5 davaleba
-
-let emptyarr = [];
-for (var i = 1; i < 10; i++) {
-  emptyarr.push(i * i);
-  console.log(emptyarr)
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  console.log(num);
 }
+random(1, 100);
 
+
+// 5. შექმენით ობიექტების 5 ელემენტიანი მასივი, სადაც თითოეულ ობიექტს გააჩნია name და age ველები. შემდეგ შექმენით ფუნქცია, რომელიც პარამეტრად მიიღებს ამ მასივს და დააბრუნებს ყველაზე ახალგაზრდა ობიექტს (რომლის age ველიც არის უმცირესი).
+
+
+
+
+
+
+
+// 6 დაწერეთ ფუნქცია, getCurrencySymbolFromCode რომელიც პარამეტრად მიიღებს ვალუტის კოდს (USD ან EUR ან GEL) სტრინგის სახით და დააბრუნებს 
+// ამ კოდის შესაბამის სიმბოლოს ($, €, ლ) თუ ისეთი კოდს გადავცემ რომლის სიმბოლოც არ მაქვს დააბრუნოს შესაბამისი შეტყობინება
+
+
+
+// function getCurrencySymbolFromCode(a, b, c) {
+//   if (getCurrencySymbolFromCode a == "GEL") {
+//     console.log("L");
+//   } else {
+//     console.log("NOT FOUND");
+//   }
+// }
+// getCurrencySymbolFromCode('GEL', 'USD', 'EUR', 'NOK');
+
+
+// function sum(a, b) {
+// 	// var myVar = "var";
+// 	a++;
+// 	b++;
+// 	// console.log(a, b);
+// 	if (a > 0) {
+// 		return a + b;
+// 	} else {
+// 		return a + b;
+// 	}
+
+// 	console.log(a + b);
+// }
